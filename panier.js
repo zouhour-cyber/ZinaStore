6e7adadfd636091b64bfa8b9a59c62b51ad6c49a
zouhourhamza <zouhourhamzasimplonenda@gmail.com>
2021-04-14 12:45:18 +0100

    modification panier

--- panier.js
+++ panier.js
@@ -1,4 +1,17 @@
 
+//Nightmode
+
+//Récupérer le button nightmode
+let nightMode=document.getElementById('nightMode');
+
+//Ecouter l'evènement 
+nightMode.addEventListener("click",tonight);
+ // Exécution de ce code lors d'un click
+function tonight(){
+
+    document.body.classList.toggle("dark-theme");
+     }  
+    
 //loalstorage
 
 var cart = document.querySelectorAll('.add-to-cart')
